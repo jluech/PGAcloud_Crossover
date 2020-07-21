40d25ddd4f7e537b2ae834741f550da653715993
jluech <janik@luechinger.ch>
2020-07-21 18:01:49 +0200

    feat: implement OnePoint crossover
    refs #109

--- crossover/crossers.py
+++ crossover/crossers.py
@@ -1,4 +1,8 @@
+import random
 from abc import ABC, abstractmethod
+
+from population.individual import Individual
+from population.pair import Pair
 
 
 class AbstractCrossover(ABC):
@@ -11,7 +15,20 @@ class AbstractCrossover(ABC):
 
 class OnePointCrossover(AbstractCrossover):
     def perform_crossover(self, individual1, individual2):
-        pass
+        solution_length = individual1.solution.__len__()
+        if solution_length != individual2.solution.__len__():
+            raise Exception("Crossover aborted: individuals solution strings are of different length!")
+
+        crossover_point = random.randint(1, solution_length-1)
+        crossed1 = Individual("{ind1_}{ind2_}".format(
+            ind1_=individual1.solution[:crossover_point],  # first part of 1
+            ind2_=individual2.solution[crossover_point:],  # second part of 2
+        ))
+        crossed2 = Individual("{ind2_}{ind1_}".format(
+            ind2_=individual2.solution[:crossover_point],  # first part of 2
+            ind1_=individual1.solution[crossover_point:],  # second part of 1
+        ))
+        return Pair(crossed1, crossed2)
 
 
 class MultiPointCrossover(AbstractCrossover):
