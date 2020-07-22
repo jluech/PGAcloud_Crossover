69471783aadd5ec4547fbc4ed23d442b4a33b7f0
jluech <janik@luechinger.ch>
2020-07-22 16:56:00 +0200

    feat: add redis handler
    refs #109

--- crossover/__main__.py
+++ crossover/__main__.py
@@ -1,6 +1,13 @@
+import logging
+
+from database_handler.handlers import DatabaseHandlers
+from database_handler.redis_handler import RedisHandler
 from message_handler.rabbit_message_queue import RabbitMessageQueue
 from utilities.utils import MessageHandlers, Crossers, __set_crosser, get_pga_id, set_property
 
+logging.basicConfig(level=logging.DEBUG)  # TODO: remove and reduce to INFO
+
+DATABASE_HANDLER = DatabaseHandlers.Redis
 MESSAGE_HANDLER = MessageHandlers.RabbitMQ
 CROSSER = Crossers.OnePoint
 RELEVANT_PROPERTIES = ["CROSSOVER_RATE"]
@@ -8,10 +15,22 @@ RELEVANT_PROPERTIES = ["CROSSOVER_RATE"]
 
 def listen_for_crossover():
     pga_id = get_pga_id()
+    database_handler = get_database_handler(pga_id)
+    for prop in RELEVANT_PROPERTIES:
+        set_property(
+            property_key=prop,
+            property_value=database_handler.retrieve(prop)
+        )
 
     message_handler = get_message_handler(pga_id)
     message_handler.receive_messages()
 
+
+def get_database_handler(pga_id):
+    if DATABASE_HANDLER == DatabaseHandlers.Redis:
+        return RedisHandler(pga_id)
+    else:
+        raise Exception("No valid DatabaseHandler defined!")
 
 
 def get_message_handler(pga_id):
