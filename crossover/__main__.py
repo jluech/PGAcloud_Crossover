15cc44521fa65a98e81d906f3fad58ded9fa87cd
jluech <janik@luechinger.ch>
2020-07-22 16:45:07 +0200

    feat: retrieve PGA ID and allow forwarding properties
    refs #109

--- crossover/__main__.py
+++ crossover/__main__.py
@@ -1,18 +1,22 @@
 from message_handler.rabbit_message_queue import RabbitMessageQueue
-from utilities.utils import MessageHandlers, Crossers, __set_crosser
+from utilities.utils import MessageHandlers, Crossers, __set_crosser, get_pga_id, set_property
 
 MESSAGE_HANDLER = MessageHandlers.RabbitMQ
 CROSSER = Crossers.OnePoint
+RELEVANT_PROPERTIES = ["CROSSOVER_RATE"]
 
 
 def listen_for_crossover():
-    message_handler = get_message_handler()
+    pga_id = get_pga_id()
+
+    message_handler = get_message_handler(pga_id)
     message_handler.receive_messages()
 
 
-def get_message_handler():
+
+def get_message_handler(pga_id):
     if MESSAGE_HANDLER == MessageHandlers.RabbitMQ:
-        return RabbitMessageQueue()
+        return RabbitMessageQueue(pga_id)
     else:
         raise Exception("No valid MessageHandler defined!")
 
